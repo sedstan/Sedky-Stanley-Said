20325ebcff4e1e40bb17d93bbd9a937d664fab82
Sedky Said <sed@sedkystanleysaid.com>
2021-08-17 20:55:26 +0100

    about-me section with styles

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,7 +1,35 @@
 <template>
-    <section id="about-me">
-        <h2 class="text-2xl md:text-4xl lg:text-6xl mb-4">About Me</h2>
-       
+    <section id="about-me" class="bg-bistro">
+        <div class="container-fluid">
+            <h2 class="font-titillium font-black text-2xl md:text-4xl lg:text-6xl text-oldLace mb-5 py-4 md:py-6 lg:py-8">
+                About Me
+            </h2>
+        </div>
+        <div class="container">
+            <div class="sm:py-5 sm:max-w-full md:max-w-2xl lg:max-w-3xl mx-auto text-left font-roboto text-cultured" >
+                <p class="mb-3">
+                    I am an ambitious Frontend developer with a fondness for any
+                    progressive framework looking to move into a frontend role
+                    that uses Vue or React. In my previous tenure, I have gained
+                    an eye for detail through the use of semantic HTML, CSS BEM
+                    methodologies and components when using progressive
+                    frameworks. I’m seeking a role with a team of developers I
+                    can learn from whilst providing my knowledge and skillsets
+                    to others.
+                </p>
+                <p class="mb-3">
+                    Upon arrival to the UK in 2014, I worked as photographer
+                    assistant and video camera operator whilst working toward a
+                    career in tech.
+                </p>
+
+                <p class="mb-3">
+                    Prior to working in the UK, I worked as a retail employee
+                    and video camera operator for a local government television
+                    station.
+                </p>
+            </div>
+        </div>
     </section>
 </template>
 
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,11 +1,12 @@
 <template>
     <section id="projects" class="bg-copper">
-        <div class="container-fluid">
+        <div class="container-fluid font-titillium text-oldLace mb-5 py-4 md:py-6 lg:py-8">
             <h2
-                class="font-titillium font-black text-oldLace text-2xl md:text-4xl lg:text-6xl mb-5"
+                class="font-black  text-2xl md:text-4xl lg:text-6xl mb-5"
             >
                 Projects
             </h2>
+            <h4 class="font-bold text-lg md:text-xl lg:text-2xl">My last 10 repos</h4>
         </div>
         <div class="container">
             <Carousel />
