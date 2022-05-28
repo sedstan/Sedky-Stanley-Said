c633e503a6cd47202b0ae4837751f0059d8a9722
Sedky Said <sed@sedkystanleysaid.com>
2022-05-28 22:08:02 +0100

    Update hero elements --add explicit height and width to bg img --add explicit height and width to headshot img

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,12 +3,16 @@
         <img
             src="https://sed-stan-personal-site.s3.amazonaws.com/images/laptop-stickers-1920.png"
             alt="A laptop with stickers on it."
+            width="1920"
+            height="1440"
         />
         <div class="container absolute font-titillium text-oldLace pb-12">
             <img
                 class="border border-oldLace bg-barnRed rounded-full w-24 md:w-36 lg:w-40 h-auto mx-auto mb-9 mt-10 md:mt-0"
                 src="https://sed-stan-personal-site.s3.amazonaws.com/images/head-shot-transparent.png"
                 alt="A headshot of a man."
+                width="160"
+                height="160"
             />
             <h1 class="font-black text-3xl md:text-6xl lg:text-7xl mb-3">
                 {{ name }}
