1afd7ed654b907614ed0c9a0a0375dbfaa17c161
Sedky Said <sed@sedkystanleysaid.com>
2022-11-02 07:48:14 +0000

    move logo img source into a promise

---
+++ src/components/HeaderSkeleton.vue
@@ -0,0 +1,10 @@
+<template>
+    <header class="header bg-white text-oldLace hidden lg:flex lg:w-full lg:fixed lg:shadow-sm z-10 w-full">
+        <div class="lg:w-2/12 lg:justify-start">
+            <img  class="header__logo"/>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+</style>
