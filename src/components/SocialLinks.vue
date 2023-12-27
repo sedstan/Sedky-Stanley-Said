e412b3db44c2b4c1116ec21c1fa312eba5416816
Sedky Said <sed@sedkystanleysaid.com>
2023-12-27 08:01:03 +0000

    remove twitter link

--- src/components/SocialLinks.vue
+++ src/components/SocialLinks.vue
@@ -3,80 +3,29 @@
 <template>
     <ul class="container flex flex-wrap justify-center items-center">
         <li class="mr-3">
-            <a
-                href="https://github.com/sedstan"
-                referrerpolicy="no-referrer"
-                rel="external"
-                target="_blank"
-                ><img
+            <a href="https://github.com/sedstan" referrerpolicy="no-referrer" rel="external" target="_blank"><img
                     class="inline-block w-1/2 md:w-2/3 lg:w-full"
                     src="https://sed-stan-personal-site.s3.eu-west-1.amazonaws.com/images/github-square-brands-f092_2x.webp"
-                    alt="A GitHub icon."
-                    width="50"
-                    height="50"
-                    loading="lazy"
-                />
+                    alt="A GitHub icon." width="50" height="50" loading="lazy" />
             </a>
         </li>
         <li class="mr-3">
-            <a
-                href="https://linkedin.com/in/sstanleysaid"
-                referrerpolicy="no-referrer"
-                rel="external"
-                target="_blank"
-                ><img
+            <a href="https://linkedin.com/in/sstanleysaid" referrerpolicy="no-referrer" rel="external" target="_blank"><img
                     class="inline-block w-1/2 md:w-2/3 lg:w-full"
                     src="https://sed-stan-personal-site.s3.eu-west-1.amazonaws.com/images/linked-in_2x.webp"
-                    alt="A LinkedIn icon."
-                    width="50"
-                    height="50"
-                    loading="lazy"
-            /></a>
+                    alt="A LinkedIn icon." width="50" height="50" loading="lazy" /></a>
         </li>
         <li class="mr-3">
-            <a
-                href="https://twitter.com/sedstanleysaid"
-                referrerpolicy="no-referrer"
-                rel="external"
-                target="_blank"
-                ><img
+            <a href="https://www.facebook.com/sedstanley" referrerpolicy="no-referrer" rel="external" target="_blank"><img
                     class="inline-block w-1/2 md:w-2/3 lg:w-full"
-                    src="https://sed-stan-personal-site.s3.eu-west-1.amazonaws.com/images/twitter-square-brands-f081_2x.webp"
-                    alt="A Twitter icon."
-                    width="50"
-                    height="50"
-                    loading="lazy"
-            /></a>
-        </li>
-        <li class="mr-3">
-            <a
-                href="https://www.facebook.com/sedstanley"
-                referrerpolicy="no-referrer"
-                rel="external"
-                target="_blank"
-                ><img
-                    class="inline-block w-1/2 md:w-2/3 lg:w-full"
-                    src="https://sed-stan-personal-site.s3.amazonaws.com/images/facebook.webp"
-                    alt="A facebook icon"
-                    width="50"
-                    height="50"
-                    loading="lazy"
-            /></a>
+                    src="https://sed-stan-personal-site.s3.amazonaws.com/images/facebook.webp" alt="A facebook icon"
+                    width="50" height="50" loading="lazy" /></a>
         </li>
         <li class="">
-            <a
-                href="https://instagram.com/sedstan"
-                referrerpolicy="no-referrer"
-                rel="external"
-                target="_blank"
-                ><img
+            <a href="https://instagram.com/sedstan" referrerpolicy="no-referrer" rel="external" target="_blank"><img
                     class="inline-block w-1/2 md:w-2/3 lg:w-full"
-                    src="https://sed-stan-personal-site.s3.amazonaws.com/images/instagram.webp"
-                    alt="A instagram icon."
-                    width="50"
-                    height="50"
-                    loading="lazy"
-            /></a>
+                    src="https://sed-stan-personal-site.s3.amazonaws.com/images/instagram.webp" alt="A instagram icon."
+                    width="50" height="50" loading="lazy" /></a>
         </li>
     </ul>
 </template>
